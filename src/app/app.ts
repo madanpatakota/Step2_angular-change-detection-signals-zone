10212619e2de59269e8347ff8de5893621a3f7fb
Madan.patakota <madan.patakota@gmail.com>
2026-09-03 19:29:36 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Step2_angular-change-detection-signals-zone');
+
 }
